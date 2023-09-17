0ada4cf66faacdbdb2459d2c2ba618e09ccc461a
Yuki Sumi <79405456+sumi-yuki@users.noreply.github.com>
2023-09-17 15:57:29 +0900

    Update deeplearning-5layer-down.py

--- supplemental/deeplearning-5layer-down.py
+++ supplemental/deeplearning-5layer-down.py
@@ -184,10 +184,10 @@ plt.savefig("./training_down.jpg", dpi=2400)
 plt.show()
 
 # save the trained model for python
-model.save("deeplearing_model_down")
+model.save("deeplearning_model_down")
 # save the trained model for javascript
 import tensorflowjs as tfjs
-tfjs.converters.save_keras_model(model, "./mostgraph_jsmodel_down")
+tfjs.converters.save_keras_model(model, "./mostgraph_model_down")
 
 # test the model
 # labels_test = mostgraph_normal_test_data_numpy[ : , 0:1]
